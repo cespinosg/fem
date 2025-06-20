Fixed the calculation of the velocity magnitude.

conv_diff_2d.py
@@ -51,6 +51,12 @@ class ExponentialFlow(f2d.Mesh):
             },
         }
 
+    def vel_func(self, x, y):
+        '''
+        Returns the velocity vector at the given coordinates.
+        '''
+        return np.array([5, 0])
+
     def check_solution(self, phi):
         '''
         Checks that the numerical solution matches the analytical one.
@@ -114,11 +120,23 @@ class ExpFlowVarSource(ExponentialFlow):
         '''
         return x**2
 
+    def check_solution(self, phi):
+        '''
+        Checks that the analytical solution matches the numerical one.
+        '''
+        a = [[1, 1], [1, np.exp(200)]]
+        b = [0, -1/600-1/4e4-1/4e6]
+        c1, c2 = np.linalg.solve(a, b)
+        x = self.points[0, :self.nx]
+        analytical = c1+c2*np.exp(200*x)+1/600*x**3+1/4e4*x**2+1/4e6*x
+        check = np.allclose(phi[:self.nx], analytical, atol=1e-6)
+        print(f'Exponential flow with variable source check: {check}')
+
 
 if __name__ == '__main__':
     # mesh = ExponentialFlow(nx=10, ny=10)
-    mesh = ExpFlowVarSource(nx=2, ny=2)
+    mesh = ExpFlowVarSource(nx=30, ny=30)
     solver = f2d.Solver(mesh)
     solver.write('results/conv_diff_2d/solution.vts')
-    # mesh.check_solution(solver.phi)
+    mesh.check_solution(solver.phi)
 

fem_2d.py
@@ -189,7 +189,7 @@ class QuadElement:
         Sets the velocity at the Gauss points.
         '''
         self.u = np.array([vel_func(*g) for g in self.gauss_points.T]).T
-        self.mag_u = np.sqrt(np.sum(self.u, axis=0))
+        self.mag_u = np.sqrt(np.sum(self.u**2, axis=0))
         self._calculate_convective_derivative()
         self._calculate_weighting_function()
 
@@ -214,11 +214,6 @@ class QuadElement:
                 pe = 0.5*self.mag_u[i]*h/self.k[i]
                 alpha = 1/np.tanh(pe)-1/pe
                 self.w[i] = n+alpha*0.5*h*self.conv_der[i]/self.mag_u[i]
-                print(f'mag_u = {self.mag_u[i]}')
-                print(f'h = {h}')
-                print(f'k = {self.k[i]}')
-                print(f'pe = {pe}')
-                print(f'alpha = {alpha}')
             else:
                 self.w[i] = n
 

test_fem_2d.py
@@ -22,15 +22,14 @@ def test_quad_element():
     diff_check = np.isclose(d[0, 1], -1/6)
     print(f'\nD_12 check: {diff_check}')
 
-    pe = 200
+    pe = 100
     local_pe = 0.5*pe
     alpha = 1/np.tanh(local_pe)-1/local_pe
-
     vel_func = lambda x, y: np.array([pe, 0])
     quad.set_velocity(vel_func)
     c = quad.calculate_convection()
     print(f'\nc = {c}')
-    conv_check = np.isclose(c[0, 1], 1/6*(1-alpha))
+    conv_check = np.isclose(c[0, 1], pe*1/6*(1-alpha))
     print(f'\nC_12 check: {conv_check}')
 
     source_func = lambda x, y: x**2